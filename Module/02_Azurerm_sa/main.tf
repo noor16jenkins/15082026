226a3d8e41c8d631779f6db90a8e57d5939b183a
noor16jenkins <40912077+noor16jenkins@users.noreply.github.com>
2026-08-19 21:40:09 +0530

    Checkov_removed

--- Module/02_Azurerm_sa/main.tf
+++ Module/02_Azurerm_sa/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "SA08" {
-  for_each                 = var.SA
+  for_each= var.SA
   name                     = each.value.name
   location                 = each.value.location
   resource_group_name      = each.value.resource_group_name
